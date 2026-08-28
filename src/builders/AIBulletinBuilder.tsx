7436f3a9fefa7e81d2edad729751d3a4a5b40a78
Webster2316 <alaina2317@gmail.com>
2026-08-28 16:45:28 +0800

    Adding imports

--- src/builders/AIBulletinBuilder.tsx
+++ src/builders/AIBulletinBuilder.tsx
@@ -1,5 +1,13 @@
 import {useState, UseEffect } from "react";
 import { Plus, Trash2, Copy, Check, Save, Eye, Code2, BookOpen, Archive, Megaphone, Loader2, RotateCcw, Files, ArrowRightLeft } from "lucide-react";
+import Field from "../shared/field";
+import MoveButtons from "../shared/moveButtons";
+import RecordsPanel from "../shared/recordsPanel";
+import RecordViewer from "../shared/recordViewer";
+import { uid, esc, inputCls } from "../shared/utils";
+
+
+const FONT = "'Yu Gothic UI','Yu Gothic','Meiryo','Segoe UI',Arial,sans-serif";
 
 export default function AIBulletinBuilder() {
   return <div>Work in progress</div>;
