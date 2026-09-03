6e2a1b6439687e6aff743b91edb7d21706d9c4de
Webster2316 <alaina2317@gmail.com>
2026-09-03 14:57:59 +0800

    trying t ocommit

--- src/builders/EventsBuilder.tsx
+++ src/builders/EventsBuilder.tsx
@@ -1,3 +1,13 @@
+import { useState, useEffect } from "react";
+import { Plus, Copy, Check, Save, Eye, Code2, BookOpen, GraduationCap, TrendingUp, Archive, Loader2, RotateCcw, Trash2 } from "lucide-react";
+import Field from "../shared/field";
+import MoveButtons from "../shared/moveButtons";
+import RecordsPanel from "../shared/recordsPanel";
+import RecordViewer from "../shared/recordViewer";
+import RichTextEditor from "../shared/richTextEditor";
+import { uid, esc, inputCls } from "../shared/utils";
+import DocumentUploadModal from "../shared/documentUploadModal";
+
 export default function EventsBuilder() {
     <div>
         <p>Work in progress</p>
