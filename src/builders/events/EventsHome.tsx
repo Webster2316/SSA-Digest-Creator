8189edd7a287368f42875b3c5ddd050727cbadbb
Webster2316 <alaina2317@gmail.com>
2026-09-08 17:45:11 +0800

    fixing positiioning

--- src/builders/events/EventsHome.tsx
+++ src/builders/events/EventsHome.tsx
@@ -20,7 +20,9 @@ const statusOptions: Record<string, { text: string; bg: string; color: string; b
 export default function EventsHome() {
     const [events, setEvents] = useState(makeEventItem);
 return(
-    <div>
+    <div className="min-h-screen bg-gray-100">
+    <div className="max-w-4xl mx-auto p-4">
+      <div className="flex items-center justify-between mb-3">
     <div className="flex items-center gap-3">
     <img
       src="https://raw.githubusercontent.com/Webster2316/SSA-Digest-Creator/786c7c8a8272d594be20ad4a9e1a159363ce0002/Logo/SSA%20logo.png"
@@ -49,6 +51,8 @@ return(
               >
                 <Plus size={16} /> Add Event
               </button>
+</div>
+</div>
 </div>
 </div>
 );
