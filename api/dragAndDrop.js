0f187537fd63f8c7707b389fda55bf283754dcbd
Webster2316 <alaina2317@gmail.com>
2026-09-21 11:46:19 +0530

    forgot to integrate into Api

--- api/dragAndDrop.js
+++ api/dragAndDrop.js
@@ -14,6 +14,7 @@ export default async function handler(req, res) {
       "ai-bulletin-data",
       "events-builder-data",
       "ssa-digest-data",
+      "posts-draft-data",
     ];
     
     if (!allowedKeys.includes(key)) {
